5189230c145b1c95919e7550cdb3d9504aba28b4
Yigong Liu <ygl.code@gmail.com>
2022-11-11 10:54:15 -0800

    update

--- container-util.go
+++ container-util.go
@@ -7,17 +7,17 @@ import (
 	"container/heap"
 )
 
-type Comparator[T any] func(T, T) bool
+type LessFunc[T any] func(T, T) bool
 
 // Slice wrapper type to work with "sort" pkg
 type Slice[T any] struct {
-	Data    []T
-	Compare Comparator[T]
+	Data     []T
+	LessFunc LessFunc[T]
 }
 
 func (s Slice[T]) Len() int { return len(s.Data) }
 func (s Slice[T]) Less(i, j int) bool {
-	return s.Compare(s.Data[i], s.Data[j])
+	return s.LessFunc(s.Data[i], s.Data[j])
 }
 func (s Slice[T]) Swap(i, j int) {
 	s.Data[i], s.Data[j] = s.Data[j], s.Data[i]
@@ -28,28 +28,28 @@ type Heap[T any] struct {
 	Slice[T]
 }
 
-func NewHeap[T any](c Comparator[T], Data ...[]T) *Heap[T] {
-	h := &Heap[T]{Slice[T]{Compare: c}}
-	if len(Data) > 0 {
-		h.Slice.Data = Data[0]
+func NewHeap[T any](c LessFunc[T], data ...[]T) *Heap[T] {
+	h := &Heap[T]{Slice[T]{LessFunc: c}}
+	if len(data) > 0 {
+		h.Data = data[0]
 		heap.Init(h)
 	}
 	return h
 }
 
 func (h *Heap[T]) Push(x any) {
-	h.Slice.Data = append(h.Slice.Data, x.(T))
+	h.Data = append(h.Data, x.(T))
 }
 
 func (h *Heap[T]) Pop() any {
-	l := len(h.Slice.Data) - 1
-	x := h.Slice.Data[l]
-	h.Slice.Data = h.Slice.Data[:l]
+	l := len(h.Data) - 1
+	x := h.Data[l]
+	h.Data = h.Data[:l]
 	return x
 }
 
 func (h Heap[T]) Peek() T {
-	return h.Slice.Data[0]
+	return h.Data[0]
 }
 
 // HeapWithIndex works with container/heap while
@@ -61,15 +61,15 @@ type HeapWithIndex[T comparable] struct {
 	indices map[T]int
 }
 
-func NewHeapWithIndex[T comparable](c Comparator[T], Data ...[]T) *HeapWithIndex[T] {
+func NewHeapWithIndex[T comparable](c LessFunc[T], data ...[]T) *HeapWithIndex[T] {
 	h := &HeapWithIndex[T]{
-		Heap[T]{Slice[T]{Compare: c}},
+		Heap[T]{Slice[T]{LessFunc: c}},
 		make(map[T]int),
 	}
-	if len(Data) > 0 {
-		h.Slice.Data = Data[0]
-		for i := 0; i < len(h.Slice.Data); i++ {
-			h.indices[h.Slice.Data[i]] = i
+	if len(data) > 0 {
+		h.Data = data[0]
+		for i := 0; i < len(h.Data); i++ {
+			h.indices[h.Data[i]] = i
 		}
 		heap.Init(h)
 	}
@@ -89,8 +89,8 @@ func (h *HeapWithIndex[T]) Pop() any {
 
 func (h HeapWithIndex[T]) Swap(i, j int) {
 	h.Slice.Swap(i, j)
-	h.indices[h.Slice.Data[i]] = i
-	h.indices[h.Slice.Data[j]] = j
+	h.indices[h.Data[i]] = i
+	h.indices[h.Data[j]] = j
 }
 
 func (h HeapWithIndex[T]) Index(x T) int {
